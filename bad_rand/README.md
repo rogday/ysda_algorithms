375038d73c7ef746956a10ffbada264eebe70b61
rogday <s.e.a.98@yandex.ru>
2021-01-22 01:58:39 +0300

    random 30 seconds speedup like wtf

--- bad_rand/README.md
+++ bad_rand/README.md
@@ -10,4 +10,4 @@ but doesn't matter). The task is to guess SEED.
 glibc's rand() is implemented as linear-feedback shift register with state size of 31. So, it is possible to express
 the state after i iterations by multiplying initial state by matrix of coefficients. First, we find such matrix by using
 the repeated custom_rand() calls with vectors of coefficients instead of actual numbers. Then it's just parallelized
-brute-force, which covers u32 range in about 1.5 minutes on my machine.
+brute-force, which covers u32 range in about 40 seconds on my machine.
--- bad_rand/src/main.rs
+++ bad_rand/src/main.rs
@@ -1,11 +1,15 @@
 use rayon::prelude::*;
-use std::cell::RefCell;
 
 fn main() {
     const TARGET: &[u8; 10] = b"NLXGI4NoAp";
     const ALPHABET: &[u8; 62] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     const FORWARD_BY: usize = 100_000_000 + 310;
     const STATE_SIZE: usize = 31;
+    const MODULO: i64 = 2_147_483_647;
+
+    let mut pows: [i64; STATE_SIZE] = [1; STATE_SIZE];
+    (1..STATE_SIZE).for_each(|i| pows[i] = (pows[i - 1] * 16_807) % MODULO);
+    let pows = pows;
 
     let mut forward_matrix = [[0u32; STATE_SIZE]; STATE_SIZE];
     (0..STATE_SIZE).for_each(|i| forward_matrix[i][i] = 1);
@@ -14,7 +18,9 @@ fn main() {
     let mut end = 0;
 
     for _ in 0..FORWARD_BY {
-        (0..STATE_SIZE).for_each(|j| forward_matrix[begin][j] += forward_matrix[end][j]);
+        (0..STATE_SIZE).for_each(|j| {
+            forward_matrix[begin][j] = forward_matrix[begin][j].wrapping_add(forward_matrix[end][j])
+        });
         begin = (begin + 1) % STATE_SIZE;
         end = (end + 1) % STATE_SIZE;
     }
@@ -24,30 +30,23 @@ fn main() {
             println!("{:?}: {}M", std::thread::current().id(), seed / 1_000_000);
         }
 
-        thread_local! {
-            static INNER_STATE: RefCell<[i32; STATE_SIZE]> = RefCell::new([0i32; STATE_SIZE]);
+        for i in 0..TARGET.len() {
+            let coeffs = &forward_matrix[(begin + i) - TARGET.len()];
+            let first_part = coeffs[0].wrapping_mul(((seed as i32 as i64) * pows[0]) as u32);
+
+            let sum: u32 = coeffs
+                .iter()
+                .zip(pows.iter())
+                .skip(1)
+                .map(|(x, y)| x.wrapping_mul((((seed as i64) * y) % MODULO) as u32))
+                .fold(0u32, |acc, x| acc.wrapping_add(x))
+                .wrapping_add(first_part);
+
+            if TARGET[i] != ALPHABET[(sum >> 1) as usize % ALPHABET.len()] {
+                return;
+            }
         }
 
-        INNER_STATE.with(|state| {
-            let mut word = seed as i64;
-            for x in state.borrow_mut().iter_mut() {
-                *x = word as i32;
-                word = (16_807 * word) % 2_147_483_647;
-            }
-
-            for i in 0..TARGET.len() {
-                let sum: u32 = forward_matrix[begin - TARGET.len() + i]
-                    .iter()
-                    .zip(state.borrow().iter())
-                    .map(|(x, &y)| x * (y as u32))
-                    .sum();
-
-                if TARGET[i] != ALPHABET[(sum >> 1) as usize % ALPHABET.len()] {
-                    return;
-                }
-            }
-
-            println!("\n{:?} found: {}\n", std::thread::current().id(), seed);
-        });
+        println!("\n{:?} found: {}\n", std::thread::current().id(), seed);
     });
 }
